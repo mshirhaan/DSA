2c38e565777510c6ab87d1106fed633bcfcf2ca2
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-06-11 10:02:50 +0530

    Update DailyTemperature.java

--- DailyTemperature.java
+++ DailyTemperature.java
@@ -24,3 +24,41 @@ class Solution {
         return result;
         }
 }
+
+
+//======================
+
+class Node {
+    int index;
+    int value;
+    Node(int index, int value) {
+        this.index = index;
+        this.value = value;
+    }
+}
+
+class Solution {
+    public static int[] dailyTemperatures(int[] temps) {
+        int n = temps.length;
+
+        Stack<Node> stack = new Stack<>();
+
+        int[] result = new int[temps.length];
+
+        result[n-1] = 0;
+        stack.push(new Node(n-1, temps[n-1]));
+        
+        for(int i = n-2; i>=0; i--) {
+            while(stack.size() > 0 && stack.peek().value <= temps[i]) {
+                stack.pop();
+            }
+            if(stack.size() > 0) {
+                result[i] = stack.peek().index - i; 
+            } else {
+                result[i] = 0;
+            }
+            stack.push(new Node(i, temps[i]));
+        }
+        return result;
+    }
+}
