e39925281fc49307bac645710277874cfef57fc6
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-06-17 12:12:24 +0530

    Update RemoveNthNodeFromEndOfLinkedList.java

--- RemoveNthNodeFromEndOfLinkedList.java
+++ RemoveNthNodeFromEndOfLinkedList.java
@@ -29,3 +29,63 @@ class Solution {
         return head;
     }
 }
+
+
+
+
+=============================
+//The follwoing approach is taken from linked list remove from begenning and the trick is to convertt n to size - n
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        //size = 5;
+
+        int size = 0;
+        ListNode curr = head;
+        while(curr!=null) {
+            size++;
+            curr = curr.next;
+        }
+
+        ListNode newHead = remove(head, size-n, size);
+        return newHead;
+    }
+
+    public ListNode removeFirst(ListNode head) {
+
+        if(head == null) {
+            return null;
+        }
+        ListNode curr = head;
+        head = head.next;
+        curr.next = null;
+        return head;
+    }
+
+    public ListNode remove(ListNode head, int index,  int size) {
+        if(index+1 > size) {
+            return null;
+        }
+
+        if(index == 0 || size == 1) {
+            ListNode temp = this.removeFirst(head);
+            return temp;
+        }
+
+        int count = 0;
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while(count!=index) {
+            prev = curr;
+            curr = curr.next;
+            count++;
+        }
+
+        prev.next = curr.next;
+        curr.next = null;
+        return head;
+    }
+
+}
