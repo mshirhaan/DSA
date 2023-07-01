29b34f98ac197faf7868b44d4735ecf7adf929c0
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-07-01 11:05:03 +0530

    Create SubsetSumInfiniteOnlyOne.js

---
+++ SubsetSumInfiniteOnlyOne.js
@@ -0,0 +1,21 @@
+
+function subsets(nums, sum, index = 0, bag = [], bagSum = 0, trolly = []) {
+
+    if(bagSum > sum) {
+        return trolly;
+    }
+
+    if(bagSum == sum) {
+        trolly.push([...bag]);
+        return trolly;
+    }
+
+    if(index == nums.length) {
+        return trolly;
+    }
+    
+    bag.push(nums[index]);
+    subsets(nums, sum, index, bag, bagSum + nums[index], trolly);
+    bag.pop();
+    return subsets(nums, sum, index+1, bag, bagSum, trolly);
+}
