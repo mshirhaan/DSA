e94608d07f953da5097bbbb9a448a010922b81a0
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-07-15 11:23:19 +0530

    Create SameTree.java

---
+++ SameTree.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return dfs(p, q);
+    }
+
+    private boolean dfs(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) return false;
+
+        boolean left = dfs(p.left, q.left);
+        if(left == false) return false;
+        boolean right = dfs(p.right, q.right);
+
+        return right;
+    }
+}
