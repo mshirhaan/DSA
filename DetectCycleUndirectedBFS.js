76919f43f4944de36e8d81e2d38e411a158743c6
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-08-26 12:07:40 +0530

    Update DetectCycleUndirectedBFS.js

--- DetectCycleUndirectedBFS.js
+++ DetectCycleUndirectedBFS.js
@@ -6,35 +6,6 @@ let adjList = {
     E: ['C', 'D']
 }
 
-function detectCycleDfs(adjList) {
-    let visited = {};
-    for(let key in adjList) {
-        if(visited[key]) {
-            continue;
-        }
-        if(dfs(key , -1) == true) {
-            return true;
-        }
-    }
-    return false;
-
-    function dfs(node, parent) {
-        visited[node] = true;
-        for(let neighbour of adjList[node]) {
-            if(neighbour == parent) {
-                continue;
-            }
-            if(visited[neighbour]) {
-                return true;
-            }
-            if(dfs(neighbour, node) == true) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
 function detectCycleBfs(adjList) {
     let visited = {};
     for(let key in adjList) {
@@ -64,7 +35,5 @@ function detectCycleBfs(adjList) {
     }  
 }
 
-
-//detectCycleDfs(adjList)
 
 detectCycleBfs(adjList)
