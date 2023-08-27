cb4e94d67da026f38d13adf1bd13632da9917a95
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-08-27 11:45:42 +0530

    Create CourseScheduleKahns.js

---
+++ CourseScheduleKahns.js
@@ -0,0 +1,50 @@
+var canFinish = function(numCourses, prerequisites) {
+    let adjList = {};
+    for(let i = 0; i < numCourses; i++) {
+        adjList[i] = [];
+    }
+    for(let [course, dependency] of prerequisites) {
+      adjList[course].push(dependency);
+    }
+
+    return !topoBFSCycleDetection(adjList, numCourses);
+};
+
+//Kahn's
+function topoBFSCycleDetection(adjList, n) {
+    let inDegree = {};
+    let ans = [];
+    for(let key in adjList) {
+        inDegree[key] = 0;
+    }
+
+    for(let key in adjList) {
+        for(let dependency of adjList[key]) {
+            inDegree[dependency]++;
+        }
+    }
+
+    let queue = [];
+
+    for(let key in inDegree) {
+        if(inDegree[key] == 0) {
+            queue.push(key);
+        }
+    }
+
+    while(queue.length) {
+        let curr = queue.shift();
+        ans.push(+curr);
+        for(let dependency of adjList[curr]) {
+            inDegree[dependency]--;
+            if(inDegree[dependency] == 0) {
+                queue.push(dependency);
+            }
+        }
+    }
+
+    if(n == ans.length) {
+        return false;
+    }
+    return true;
+}
