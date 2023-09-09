69c6d5d886d1ba5b1e825fbc2b8001d248c3d32c
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2023-09-09 10:15:00 +0530

    Update UniquePaths.js

--- UniquePaths.js
+++ UniquePaths.js
@@ -1,3 +1,5 @@
+//recursion, top down
+
 var uniquePaths = function(m, n) {
     
     function rec(row, col, memo = {}) {
@@ -24,4 +26,34 @@ var uniquePaths = function(m, n) {
     let res = rec(0,0, memo);
     console.log(memo)
     return res;
+};
+
+
+
+
+// tabulation, bottom up
+var uniquePaths = function(m, n) {
+    
+    let grid = [];
+    for(let i = 0; i < m; i++) {
+        grid[i] = [];
+    }
+    grid[m-1][n-1] = 1;
+
+    for(let i = m - 1; i >= 0; i--) {
+        for(let j = n - 1; j >= 0; j--) {
+            if(i == m -1 && j == n - 1) continue;
+
+            grid[i][j] = 0;
+
+            if(i+1 < m) {
+                grid[i][j]+= grid[i+1][j];
+            }
+            if(j+1 < n) {
+                grid[i][j]+= grid[i][j+1];
+            }
+        }
+    }
+    return grid[0][0];
+    
 };
